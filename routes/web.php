6fbecca8ec777ea93e38e8bde417780693544216
Adonis Imperial <adonisimperial@yahoo.com>
2025-01-30 10:02:39 +0800

    viewDataAndRouterWildcardsIntoClass

--- routes/web.php
+++ routes/web.php
@@ -3,72 +3,51 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+class Job {
+
+    public static function all(): array {
+        return [
+            [
+                "id" => 1,
+                "title" => "Director",
+                "salary" => "$20,000"
+            ],
+            [
+                "id" => 2,
+                "title" => "Software Developer",
+                "salary" => "$15,000"
+            ],
+            [
+                "id" => 3,
+                "title" => "Web Developer",
+                "salary" => "$10,000"
+            ],
+            [
+                "id" => 4,
+                "title" => "Mobile App Developer",
+                "salary" => "$11,000"
+            ],
+            [
+                "id" => 5,
+                "title" => "Game Developer",
+                "salary" => "$10,500"
+            ]
+        ];
+    }
+}
+
 Route::get("/", function () {
     return view("home");
 });
 
 Route::get("/jobs", function () {
     return view("jobs", [
-        "jobs" => [
-            [
-                "id" => 1,
-                "title" => "Director",
-                "salary" => "$20,000"
-            ],
-            [
-                "id" => 2,
-                "title" => "Software Developer",
-                "salary" => "$15,000"
-            ],
-            [
-                "id" => 3,
-                "title" => "Web Developer",
-                "salary" => "$10,000"
-            ],
-            [
-                "id" => 4,
-                "title" => "Mobile App Developer",
-                "salary" => "$11,000"
-            ],
-            [
-                "id" => 5,
-                "title" => "Game Developer",
-                "salary" => "$10,500"
-            ]
-        ]
+        "jobs" =>  Job::all()
     ]);
 });
 
 Route::get("/jobs/{id}", function ($id) {
-    $jobs = [
-        [
-                "id" => 1,
-                "title" => "Director",
-                "salary" => "$20,000"
-            ],
-            [
-                "id" => 2,
-                "title" => "Software Developer",
-                "salary" => "$15,000"
-            ],
-            [
-                "id" => 3,
-                "title" => "Web Developer",
-                "salary" => "$10,000"
-            ],
-            [
-                "id" => 4,
-                "title" => "Mobile App Developer",
-                "salary" => "$11,000"
-            ],
-            [
-                "id" => 5,
-                "title" => "Game Developer",
-                "salary" => "$10,500"
-            ]
-    ];
-
-    $job = Arr::first($jobs, fn($job) => $job["id"] == $id);
+    $job = Arr::first(Job::all(), fn($job) => $job["id"] == $id);
     /*
     $job = Arr::first($jobs, function ($job) use ($id) {
         return $job["id"] == $id;
