a05361d3a87f781d58564feb14967dc5f2de2ed6
Adonis Imperial <adonisimperial@yahoo.com>
2025-02-13 21:44:27 +0800

    editUpdateDeleteJob

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,9 +3,20 @@
         Job Listings
     </x-slot:heading>
 
-    <h2 class="font-bold text-lg">{{ $job["title"] }}</h2>
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
 
     <p>
-        This job pays {{ $job["salary"] }} per year.
+        This job pays {{ $job->salary }} per year.
     </p>
+
+    <p class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+        <!-- sample for form attribute -->
+        <button form="delete-form" class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Delete</button>
+    </p>
+
+    <form action="/jobs/{{ $job->id }}" method="POST" id="delete-form" class="hidden">
+        @csrf
+        @method('DELETE')
+    </form>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->paginate(3);
 
@@ -15,16 +16,19 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function() { // Pitfalls: putting this route below the route with wildcard below will not work
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -38,6 +42,47 @@ Route::post('/jobs', function () {
         'employer_id' => 1
     ]);
 
+    return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    // authorize (on hold)
+
+    $job = Job::findOrFail($id);
+
+    /*
+    $job->title = request('title');
+    $job->salary = request('salary');
+    $job->save();
+    */
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+    
+    return redirect('/jobs/' . $id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    // authorize (on hold)
+
+    Job::findOrFail($id)->delete();
+    
     return redirect('/jobs');
 });
 
